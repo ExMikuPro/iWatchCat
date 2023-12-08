9f8eee94fc5597b479f10fb6f243bdbf93422ad3
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-12-08 22:37:44 +0800

    Initial Commit

---
+++ iWatchCat Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iWatchCat Watch App
+//
+//  Created by Hatsune Miku on 2023/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iWatchCat Watch App/iWatchCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWatchCatApp.swift
+//  iWatchCat Watch App
+//
+//  Created by Hatsune Miku on 2023/12/8.
+//
+
+import SwiftUI
+
+@main
+struct iWatchCat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
