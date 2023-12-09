c0e24eda83c3362c68ec3eaa0df839febf852720
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-12-09 23:45:20 +0800

    feat: 添加复杂功能个选择列表图标ClockKitLogo

--- iWatchCat Watch App/ComplicationController.swift
+++ iWatchCat Watch App/ComplicationController.swift
@@ -20,11 +20,24 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         return .init(date: Date(), complicationTemplate: template)
     }
     
+    // 在复杂功能列表里创建选项
     func complicationDescriptors() async -> [CLKComplicationDescriptor] {
         return [
             .init(identifier: "layer.practice.iWatchCat",
                   displayName: "iWatchCat",
                   supportedFamilies: [.graphicCircular])
         ]
+    }
+    
+    // 设置复杂功能选择列表图标
+    func localizableSampleTemplate(
+        for complication: CLKComplication
+    ) async -> CLKComplicationTemplate? {
+        // 使用名为ClockKitLogo的图片用作图标
+        if let fullColorImage = UIImage(named: "ClockKitLogo") {
+            let imageProvider = CLKFullColorImageProvider(fullColorImage: fullColorImage) // 用一张图片全部填满整个图标区域
+            return CLKComplicationTemplateGraphicCircularImage(imageProvider: imageProvider)
+        }
+        return nil
     }
 }
